684c340940ce881e1350fedf54a527525456f0d8
Lucas Ribeiro <luucasnck@gmail.com>
2019-09-03 08:40:01 -0300

    project mockup without contact form

--- src/components/academic/Academic.jsx
+++ src/components/academic/Academic.jsx
@@ -9,6 +9,7 @@ export default () => {
                     <ul className="row">
                         <li className="col-3">
                             <div className="item">
+                                <div className="item-background"></div>
                                 <div className="image">
                                     <span class="icon icon-graduate"></span>
                                 </div>
@@ -25,54 +26,60 @@ export default () => {
                             </div>
                         </li>
                         <li className="col-3">
-                            <div className="image">
-                                <span class="icon icon-school"></span>
+                            <div className="item">
+                                <div className="item-background"></div>
+                                <div className="image">
+                                    <span class="icon icon-graduate"></span>
+                                </div>
+                                <h2>Faculdade de Tecnologia de Lins - Prof. Antônio Seabra</h2>
+                                <div className="course">
+                                    <p>Análise e Desenvolvimento de Sistemas</p>
+                                </div>
+                                <div className="endDate">
+                                    <span className="date">06/2016</span>
+                                </div>
+                                <div className="location">
+                                    <span className="city">Lins</span><span className="state">/SP</span>
+                                </div>
                             </div>
-                            <h2>CETEC - Caxias do Sul/RS</h2>
-                            <p>Ensino Médio Técnico em Informática</p>
                         </li>
                         <li className="col-3">
-                            <div className="image">
-                                <span class="icon icon-school"></span>
+                            <div className="item">
+                                <div className="item-background"></div>
+                                <div className="image">
+                                    <span class="icon icon-graduate"></span>
+                                </div>
+                                <h2>Faculdade de Tecnologia de Lins - Prof. Antônio Seabra</h2>
+                                <div className="course">
+                                    <p>Análise e Desenvolvimento de Sistemas</p>
+                                </div>
+                                <div className="endDate">
+                                    <span className="date">06/2016</span>
+                                </div>
+                                <div className="location">
+                                    <span className="city">Lins</span><span className="state">/SP</span>
+                                </div>
                             </div>
-                            <h2>EE 21 de Abril - Lins/SP</h2>
-                            <p>Ensino Médio</p>
                         </li>
                         <li className="col-3">
-                            <div className="image">
-                                <span class="icon icon-course"></span>
+                            <div className="item">
+                                <div className="item-background"></div>
+                                <div className="image">
+                                    <span class="icon icon-graduate"></span>
+                                </div>
+                                <h2>Faculdade de Tecnologia de Lins - Prof. Antônio Seabra</h2>
+                                <div className="course">
+                                    <p>Análise e Desenvolvimento de Sistemas</p>
+                                </div>
+                                <div className="endDate">
+                                    <span className="date">06/2016</span>
+                                </div>
+                                <div className="location">
+                                    <span className="city">Lins</span><span className="state">/SP</span>
+                                </div>
                             </div>
-                            <h2>Udemy</h2>
-                            <p>Java Web Developer's masterclass : Java + JSP + JSF</p>
                         </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>Web Moderno com JavaScript! COMPLETO 2018 + Projetos</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>Complete Java Masterclass</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>Angular 6 e 7, Apollo, GraphQL e Graphcool</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>Node with React: Fullstack Web Development</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>React + Redux</p>
-                        </li>
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>API RESTful avançada com Spring Boot e Java 8</p>
-                        </li>
+                        
                     </ul>
                 </div>
             </div>
--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,15 +1,59 @@
 import React from "react"
 
+import image1 from '../../assets/img/article/fateclins.jpg'
+import image2 from '../../assets/img/article/uspsc.jpg'
+import image3 from '../../assets/img/article/uspsp.jpg'
+
 export default () => {
     return (
-        <section id="academic" className="text-center p-5">
-            <div className="academic-container">
+        <section id="articles" className="text-center p-5">
+            <div className="articles-container">
                 <div className="container">
                     <h2>Artigos Publicados</h2>
-                    <ul className="row">
-                        <li className="col-3">
-                            <h2>Udemy</h2>
-                            <p>API RESTful avançada com Spring Boot e Java 8</p>
+                    <ul>
+                        <li>
+                            <div className="item">
+                                <div className="image">
+                                    <img src={image1} />
+                                </div>
+                                <div className="content">
+                                    <h3>Gerador de Artefatos e Projetos Web</h3>
+                                    <h4>II Congresso de Iniciação Científica e Tecnológica Fatec Lins</h4>
+                                    <p>
+                                        O presente trabalho apresenta uma ferramenta web para auxiliar no desenvolvimento de aplicações JavaServer Faces automatizando a geração de artefatos e projetos, por meio de modelagem de entidades e relacionamentos, preenchimento de formulários e a utilização de elementos Drag-and-Drop. Os artefatos produzidos são baseados na arquitetura JSF: ManagedBeans, Entidades de Persistência, Data Access Object (DAO) e Interfaces de Usuário e tal ferramenta faz uso de conceitos de Fluent Interfaces e Abstract Syntax Tree (AST). Os artefatos produzidos podem ser utilizados em aplicações Web e o seu funcionamento é satisfatório. Palavras-chave:JAVASERVER FACES; GERADOR DE ARTEFATOS; AUTOMATIZAÇÃO DE CÓDIGO; SISTEMAS WEB
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <div className="item">
+                                <div className="image">
+                                    <img src={image1} />
+                                </div>
+                                <div className="content">
+                                    <h3>Gerador de Artefatos e Projetos Web</h3>
+                                    <h4>II Congresso de Iniciação Científica e Tecnológica Fatec Lins</h4>
+                                    <p>
+                                        O presente trabalho apresenta uma ferramenta web para auxiliar no desenvolvimento de aplicações JavaServer Faces automatizando a geração de artefatos e projetos, por meio de modelagem de entidades e relacionamentos, preenchimento de formulários e a utilização de elementos Drag-and-Drop. Os artefatos produzidos são baseados na arquitetura JSF: ManagedBeans, Entidades de Persistência, Data Access Object (DAO) e Interfaces de Usuário e tal ferramenta faz uso de conceitos de Fluent Interfaces e Abstract Syntax Tree (AST). Os artefatos produzidos podem ser utilizados em aplicações Web e o seu funcionamento é satisfatório. Palavras-chave:JAVASERVER FACES; GERADOR DE ARTEFATOS; AUTOMATIZAÇÃO DE CÓDIGO; SISTEMAS WEB
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                        
+                        <li>
+                            <div className="item">
+                                <div className="image">
+                                    <img src={image1} />
+                                </div>
+                                <div className="content">
+                                    <h3>Gerador de Artefatos e Projetos Web</h3>
+                                    <h4>II Congresso de Iniciação Científica e Tecnológica Fatec Lins</h4>
+                                    <p>
+                                        O presente trabalho apresenta uma ferramenta web para auxiliar no desenvolvimento de aplicações JavaServer Faces automatizando a geração de artefatos e projetos, por meio de modelagem de entidades e relacionamentos, preenchimento de formulários e a utilização de elementos Drag-and-Drop. Os artefatos produzidos são baseados na arquitetura JSF: ManagedBeans, Entidades de Persistência, Data Access Object (DAO) e Interfaces de Usuário e tal ferramenta faz uso de conceitos de Fluent Interfaces e Abstract Syntax Tree (AST). Os artefatos produzidos podem ser utilizados em aplicações Web e o seu funcionamento é satisfatório. Palavras-chave:JAVASERVER FACES; GERADOR DE ARTEFATOS; AUTOMATIZAÇÃO DE CÓDIGO; SISTEMAS WEB
+                                    </p>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
